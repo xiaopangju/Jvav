a68e9a8678fa0e093f4dd6427d8a5761bacff9ab
TheZihanGu <i@zihangu.cn>
2020-04-20 22:26:48 +0800

    Version 851c1e6

---
+++ ga/js/console.js
@@ -0,0 +1,8 @@
+/* Jvav Console.log by TheZihanGu.
+    GitHub: https://github.com/TheZihanGu.
+    Use: All page on Jvav.top
+*/
+console.log ("Jvav 网页 by TZG-Official")
+console.log("版本：%c851c1e6","color:red")
+console.log ("Powered by Netlify & Cloudflare.")
+console.log("%cGitHub: https://github.com/TZG-official/Jvav","color:red")
