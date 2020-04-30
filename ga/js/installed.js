b0c3d5841f2d9e538a159b71b92a61bf385e340d
TheZihanGu <i@zihangu.cn>
2020-04-30 09:29:39 +0800

    我有 Jvav 吗 - 新增页面

---
+++ ga/js/installed.js
@@ -0,0 +1,5 @@
+function getJvav() {
+    document.getElementById("tou_block").style.display="none";
+    document.getElementById("verify_block").style.display="none";
+    document.getElementById("jvav_block").style.display="";
+}
